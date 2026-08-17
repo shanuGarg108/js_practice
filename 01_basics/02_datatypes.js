6eacb8ed783ac54c8d778f3b2a005425b2f94811
shanuGarg108 <shanugarg2003@gmail.com>
2026-08-17 16:23:08 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict" // treat all code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+console.log(3+3); 
+// code readliablity should be high
+
+let name = "Shanu"
+let age = 23
+let isRight = true
+
+// number => 2 to power 53
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined =>
+// bigint
+// symbol => unique
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+
+
