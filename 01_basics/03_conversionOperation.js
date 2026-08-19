2c686cf85d674003b07049df744e1a27e3d8aa32
shanuGarg108 <shanugarg2003@gmail.com>
2026-08-19 15:32:03 +0000

    Conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,79 @@
+let score=124n
+
+let Numberscore = Number(score)
+// console.log(Numberscore);
+// console.log(typeof Numberscore);
+// console.log(typeof score );
+
+
+// "33" => 33 type:Number
+// true=> 1 & false=>0 type:Number
+// null => 0 type:Number
+// "33abc" => NaN type:Number
+// undefined => NaN type:Number
+// 124n => 124 type:Number
+
+let isLoggedIN = 200n
+
+let BooleanisLoggedIN = Boolean(isLoggedIN)
+// console.log(BooleanisLoggedIN);
+// console.log(typeof BooleanisLoggedIN);
+
+
+// 1=> true & 0=> false type:Boolean
+// ""=> false type:Boolean
+// "Shanu"=> true type:Boolean
+// null=> false type:Boolean
+// undefined=> false type:Boolean
+// 200=> true type:Boolean
+// 200n=> true type:Boolean
+
+let marks= null
+
+let Stringmarks = String(marks)
+// console.log(typeof Stringmarks);
+// console.log(Stringmarks);
+
+// 33=> "33" type:String
+
+
+/******************** Operations **********************/
+
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1= "Hello"
+let str2= " Shanu"
+let str3= str1 + str2
+// console.log(str3);
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+//console.log(+true);
+//console.log(+"");
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
