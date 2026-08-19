4eae30aef7bc92638b8795fe72a69fa22cab4331
shanuGarg108 <shanugarg2003@gmail.com>
2026-08-19 16:01:11 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+console.log(2>2);
+console.log(2>=2);
+console.log(2<=2);
+console.log(2==2);
+console.log(2!=2);
+
+
+console.log("2">1);
+console.log("02">1);
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+// * In javascript equality check (==) and comparisons ( > ,<,>=,<=) works differently
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+console.log("2"==2);
+console.log("2"===2);
+
