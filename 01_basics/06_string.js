96d0a0ccdb218681b7476e1312987a3361947642
shanuGarg108 <shanugarg2003@gmail.com>
2026-08-22 17:22:50 +0000

    string

---
+++ 01_basics/06_string.js
@@ -0,0 +1,43 @@
+const name="Shanu"
+let age=23
+
+// console.log(name+age);
+
+console.log(`my name is ${name} and my age is ${age}`);
+
+const gameName=new String("Shanu-gc")
+
+// console.log(gameName[3]);
+
+
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(5));
+
+// console.log(gameName.indexOf('u'));
+
+// console.log(gameName.length);
+
+const newString=gameName.substring(0,4)
+
+// console.log(newString);
+
+const anotherString=gameName.slice(-7,-3)
+
+// console.log(anotherString);
+
+const anotherStringOne="   Shanu    "
+
+// console.log(anotherStringOne);
+// console.log(anotherStringOne.trim());
+
+const url="https://shanu.com/shanu%20garg"
+
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('shanu'));
+
+console.log(gameName.split('-'));
+
